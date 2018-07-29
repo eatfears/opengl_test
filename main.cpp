eb21030549f04f8e7d61ceff6c49d623d214057c
Ilya Sivakov <ilyasivakov@gmail.com>
2018-07-29 06:35:37 +0300

    EBO

--- main.cpp
+++ main.cpp
@@ -15,11 +15,15 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 }
 
 GLfloat vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-    0.5f, -0.5f, 0.0f,
-    0.0f,  0.5f, 0.0f
+     0.5f,  0.5f, 0.0f,  // Верхний правый угол
+     0.5f, -0.5f, 0.0f,  // Нижний правый угол
+    -0.5f, -0.5f, 0.0f,  // Нижний левый угол
+    -0.5f,  0.5f, 0.0f   // Верхний левый угол
 };
-
+GLuint indices[] = {  // Помните, что мы начинаем с 0!
+    0, 1, 3,   // Первый треугольник
+    1, 2, 3    // Второй треугольник
+};
 
 int main(int argc, char *argv[])
 {
@@ -58,16 +62,23 @@ int main(int argc, char *argv[])
     glGenBuffers(1, &VBO);
     GLuint VAO;
     glGenVertexArrays(1, &VAO);
+    GLuint EBO;
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
 
     glBindVertexArray(0);
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -79,7 +90,7 @@ int main(int argc, char *argv[])
         shader.use();
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
         glfwSwapBuffers(window);
