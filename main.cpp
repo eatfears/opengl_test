0be6f85919a45a7268f37de374e0955d1f58f2d5
Ilya Sivakov <ilyasivakov@gmail.com>
2018-08-02 15:52:13 +0300

    etc

--- main.cpp
+++ main.cpp
@@ -366,6 +366,33 @@ int main()
     glEnable(GL_FRAMEBUFFER_SRGB); // gammacorrection
 #endif
     /********************************************************/
+    //shadows
+
+    unsigned int depthMapFBO;
+    glGenFramebuffers(1, &depthMapFBO);
+
+    const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
+
+    unsigned int depthMap;
+    glGenTextures(1, &depthMap);
+    glBindTexture(GL_TEXTURE_2D, depthMap);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+    glDrawBuffer(GL_NONE);
+    glReadBuffer(GL_NONE);
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    {
+        std::cout << "ERROR::FRAMEBUFFER:: Shadow framebuffer is not complete!" << std::endl;
+        exit(1);
+    }
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    /********************************************************/
 
     unsigned int tmp;
     tmp = glGetUniformBlockIndex(skyboxShader.m_Program, "Matrices");
@@ -382,10 +409,10 @@ int main()
 
     glBindBufferRange(GL_UNIFORM_BUFFER, 0, uboMatrices, 0, 2 * sizeof(glm::mat4));
     /********************************************************/
-
     //glfwWindowHint(GLFW_DOUBLEBUFFER, GL_FALSE);
     glfwSwapInterval(0);
     GLfloat frames = 0, time = 0;
+    /********************************************************/
 
     // Game loop
     while (!glfwWindowShouldClose(window))
@@ -417,7 +444,6 @@ int main()
 
 
         // Create camera transformations
-
         glBindBuffer(GL_UNIFORM_BUFFER, uboMatrices);
         glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4), glm::value_ptr(projection));
         glBindBuffer(GL_UNIFORM_BUFFER, 0);
